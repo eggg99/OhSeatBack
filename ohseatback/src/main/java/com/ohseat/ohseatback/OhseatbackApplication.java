fa0692e86c7c65b3e737d10e3004c8f94922d1b7
Jeong <eggg99999@gmail.com>
2025-04-03 13:52:43 +0900

    initial commit

---
+++ ohseatback/src/main/java/com/ohseat/ohseatback/OhseatbackApplication.java
@@ -0,0 +1,13 @@
+package com.ohseat.ohseatback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhseatbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhseatbackApplication.class, args);
+	}
+
+}
---
+++ ohseatback/src/test/java/com/ohseat/ohseatback/OhseatbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohseat.ohseatback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhseatbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
